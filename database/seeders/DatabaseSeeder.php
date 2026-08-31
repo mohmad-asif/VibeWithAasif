67fc4d44ca4ae67b54e5dbf54c923ffcece3e59f
mohmad-asif <aasifkhanrajar@gmail.com>
2026-08-31 18:19:07 +0530

    Fix undefined variable user in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::updateOrCreate(
+        $user = \App\Models\User::updateOrCreate(
             ['email' => 'admin@vibewithaasif.com'],
             [
                 'name' => 'Aasif Khan',
